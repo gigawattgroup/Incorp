8afc25cccd4fd7cc17375727be2cbcd8d6c749a0
Brigitta B <user@Brigitta-MacBook-Pro.local>
2016-12-16 13:51:34 -0500

    Added table.php page

---
+++ builds/development/table.php
@@ -0,0 +1,47 @@
+<?php include_once('_header.php'); ?>
+
+  <div id="newsletter">
+    <div class="container">
+          <div class="col-md-12">
+            <h1>Select a Package</h1>
+          </div>
+    </div><!-- .container -->
+  </div><!-- .newsletter -->
+
+  <section id="table">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-4">
+          <h2 id="table">WHAT’S INCLUDED WITH REGISTERED AGENT SERVICE?</h2>
+          <p>For just $99 per year, you’ll receive InCorp’s address for Registered Agent service and a suite of services to help manage your business. All of this is included!</p></div>
+        <div class="col-md-4" id="table">
+          <div id="greenbg"><p>Great Deal!</p></div>
+          <div id="tablebg">
+            <p id="blueprice">$99 per year</p>
+              <ul><li class="">Registered Agent Address</li>
+                <li class="">Electronic Service of Process (SOP)</li>
+                <li class="">Customized Email Notifications</li>
+                <li class="">Online Account</li>
+                <li class="">Online Forms Library</li>
+                <li class="">Electronic Document Storage</li>
+                <li class="">Electronic Invoice Delivery & Reminders</li>
+                <li class="">Online Compliance Calendar</li>
+                <li class="">Report Due Date Reminders</li>
+                <li class="">BBB Accredited A+ Rating!</li>
+                <li class="">Live Humans!</li>
+                <li class="">Dedicated Representative</li>
+                <li class="">Online Chat Support</li>
+                <li class="">Business Hours - 12 Hours Per Day</li>
+                <li class="">Absorpotion of Tranfer Fees</li>
+                <li class="">Price Match Guarantee!</li>
+                <li class="">“Big Four” Stability</li>
+              </ul>
+            </div>
+          </div></div>
+        <div class="col-md-2"></div>
+      </div><!-- .row -->
+    </div><!-- .container -->
+  </section><!-- .table -->
+
+<?php include_once('_footer.php'); ?>
