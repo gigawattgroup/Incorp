790e8803d81e1e0a6cc48f8ee0e45fb7a6264c1d
Brigitta B <user@Brigitta-MacBook-Pro.local>
2016-12-23 11:59:55 -0500

    Kitchen-Sink
    Updates

--- builds/development/kitchen-sink.php
+++ builds/development/kitchen-sink.php
@@ -12,7 +12,7 @@
 
     <div class="container">
       <div class="row">
-        <div class="col-md-12"><h3>To obtain a registered agent in your particular state, click on the state location below!</h3>
+        <div class="col-md-12"><h2>To obtain a registered agent in your particular state, click on the state location below!</h2>
           <p>A registered agent is a person or entity, designated in the articles of incorporation / organization, who can accept legal documents and notifications from a state office on behalf of a corporate entity. All corporations and Limited-Liability Companies (LLCs) must choose a person or entity to act as their registered agent. There are few restrictions regarding who can be a registered agent other than that the person or entity must be located and available at a physical street address within the state during normal business hours. It is important that you select a reliable company when choosing your registered agent.</p>
         </div>
         <div class="col-md-6">
@@ -25,9 +25,9 @@
         <div class="row"><div class="col-md-12 text-center"><h2>Unbeatable Multi-Year Discounts!</h2></div></div>
         <div class="row"><div class="col-md-12"><p></p></div></div>
         <div class="row">
-          <div class="col-md-3">
+          <div class="col-md-3 col-sm-6 col-xs-12">
             <div id="tabledeal">
-              <div id="greenbg" class="text-center"><p>GREAT DEAL!</p></div>
+              <div id="greenbg" class="text-center center-block"><p>GREAT DEAL!</p></div>
               <div id="tablebg">
                 <div class="price-center">
                   <p id="blueprice_cents">$89.10</p>
@@ -43,17 +43,17 @@
               </div><!-- #tablebg -->
             </div><!-- #tabledeal -->
           </div><!-- col-md-3 -->
-          <div class="col-md-3">
+          <div class="col-md-3 col-sm-6 col-xs-12">
             <div id="tabledeal">
               <div id="greenbg" class="text-center"><p>GREAT DEAL!</p></div>
               <div id="tablebg">
                 <div class="price-center">
-                  <p id="blueprice_cents">$89.10</p>
+                  <p id="blueprice_cents">$80.52</p>
                   <p id="blueprice_s">Per Year</p>
                   <img src="images/bluedivider.png" alt="divider">
                 </div> <!-- #price-center -->
                 <div id="price-bullets">
-                  <p class="text-center">You save <strong>$19.10!</strong></p>
+                  <p class="text-center">You save <strong>$55.44!</strong></p>
                 </div> <!-- #price-center -->
                 <div class="price-center">
                   <button class="btn btn-md btn-info">Get Started</button>
@@ -61,17 +61,17 @@
               </div><!-- #tablebg -->
             </div><!-- #tabledeal -->
           </div><!-- col-md-3 -->
-          <div class="col-md-3">
+          <div class="col-md-3 col-sm-6 col-xs-12">
             <div id="tabledeal">
               <div id="greenbg" class="text-center"><p>GREAT DEAL!</p></div>
               <div id="tablebg">
                 <div class="price-center">
-                  <p id="blueprice_cents">$89.10</p>
+                  <p id="blueprice_cents">$73.06</p>
                   <p id="blueprice_s">Per Year</p>
                   <img src="images/bluedivider.png" alt="divider">
                 </div> <!-- #price-center -->
                 <div id="price-bullets">
-                  <p class="text-center">You save <strong>$19.10!</strong></p>
+                  <p class="text-center">You save <strong>$103.75!</strong></p>
                 </div> <!-- #price-center -->
                 <div class="price-center">
                   <button class="btn btn-md btn-info">Get Started</button>
@@ -79,17 +79,17 @@
               </div><!-- #tablebg -->
             </div><!-- #tabledeal -->
           </div><!-- col-md-3 -->
-          <div class="col-md-3">
+          <div class="col-md-3 col-sm-6 col-xs-12">
             <div id="tabledeal">
               <div id="greenbg" class="text-center"><p>GREAT DEAL!</p></div>
               <div id="tablebg">
                 <div class="price-center">
-                  <p id="blueprice_cents">$89.10</p>
+                  <p id="blueprice_cents">$66.56</p>
                   <p id="blueprice_s">Per Year</p>
                   <img src="images/bluedivider.png" alt="divider">
                 </div> <!-- #price-center -->
                 <div id="price-bullets">
-                  <p class="text-center">You save <strong>$19.10!</strong></p>
+                  <p class="text-center">You save <strong>$162.20!</strong></p>
                 </div> <!-- #price-center -->
                 <div class="price-center">
                   <button class="btn btn-md btn-info">Get Started</button>
@@ -98,8 +98,229 @@
             </div><!-- #tabledeal -->
           </div><!-- col-md-3 -->
         </div><!-- .row -->
+        <div class="row"><div class="col-md-12">
+          <p class="text-center">Our rates are already lowest in the industry. Save even more by ordering multiple years of service in advance, as low as $5.55/month (see chart).</p>
+        </div></div>
       </section>
       <div class="row"><div class="col-md-12"><hr></div></div>
+      <div class="row"><div class="col-md-12"><h2 class="text-center">Compare Entity Types</h2></div></div>
+      <div class="row"><div class="col-md-12">
+        <button type="button" class="btn btn-primary btn-sm">Limited Liability Company</button>
+        <button type="button" class="btn btn-primary btn-sm">C-Corporation</button>
+        <button type="button" class="btn btn-primary btn-sm">S-Corporation</button>
+        <button type="button" class="btn btn-primary btn-sm">Limited Liability Partnership</button>
+        <button type="button" class="btn btn-primary btn-sm">Non-Profit Corporation</button>
+        <button type="button" class="btn btn-primary btn-sm">Limited Partnership</button>
+        <button type="button" class="btn btn-primary btn-sm">Sole Proprietor</button>
+      </div></div>
+      <div class="row"><div class="col-md-12"><h3 class="text-center">Limited Liability Company</h3></div></div>
+      <div class="row"><div class="col-md-12"><hr></div></div>
+      <div class="row">
+        <div class="col-md-6">
+          <p class="text-center"><strong>Advantages</strong></p>
+          <ul>
+            <li><strong>Tax Flexibility - </strong> The IRS doesn't actually recognize an LLC as a distinct entity type so the LLC can be taxed as a Sole Proprietorship for a single member or married couple, a Partnership for an LLC with multiple members, or the LLC can opt to be taxed like a corporation.</li>
+            <li><strong>Less Paperwork - </strong>LLC's have less required paperwork than C-Corps and S-Corps. The Operating Agreement and state laws govern the LLC.</li>
+            <li><strong>Limited Liability - </strong>Like corporations, LLC's provide the owners with protection from personal liability.</li>
+            <li><strong>Ownership Flexibility - </strong> Unlike the S-Corp, the owners of the LLC are not subject to limitations regarding citizenship. The LLC can also list other entities as owners including Corporations, other LLC's and Trusts.</li>
+            <li><strong>Avoid "Double Taxation" - </strong>Unless the LLC chooses to be taxed as a corporation, the profits flow through to the owners. This means there is no taxation on the entity level and only on the personal income level.</li>
+          </ul>
+        </div>
+        <div class="col-md-6">
+          <p class="text-center"><strong>Disadvantages</strong></p>
+          <ul>
+            <li><strong>Self-Employment Taxes - </strong> Unless the LLC elects to be taxed like a coporation, the profits that flow through to the members are often taxed at a higher rate than they would be at a corporate level.</li>
+            <li><strong>Limited Life - </strong>In many states the LLC can elect to exist "perpetually" when filing the original articles. In other cases the LLC has an expiration date or ceases to exist when a member departs. This can often be resolved by specifying the handling of these events in the Operating Agreement.</li>
+            <li><strong>Confusing Structure - </strong>Managers, Members, Managing Members and so on are not defined as well as the Officers and Directors of a corporation. This can lead to confusion unless it is clarified in the Operating Agreement.</li>
+          </ul>
+      </div><!-- .row -->
+      <div class="row"><div class="col-md-12"><hr></div></div>
+      <div class="row">
+        <div class="col-md-12"><h2 class="text-center">Glossary</h2>
+          <p class="text-center">
+              <a href="">A</a> |
+              <a href="">B</a> |
+              <a href="">C</a> |
+              <a href="">D</a> |
+              <a href="">E</a> |
+              <a href="">F</a> |
+              <a href="">G</a> |
+              <a href="">H</a> |
+              <a href="">I</a> |
+              <a href="">J</a> |
+              <a href="">K</a> |
+              <a href="">L</a> |
+              <a href="">M</a> |
+              <a href="">N</a> |
+              <a href="">O</a> |
+              <a href="">P</a> |
+              <a href="">Q</a> |
+              <a href="">R</a> |
+              <a href="">S</a> |
+              <a href="">T</a> |
+              <a href="">U</a> |
+              <a href="">V</a> |
+              <a href="">W</a> |
+              <a href="">X</a> |
+              <a href="">Y</a> |
+              <a href="">Z</a></p>
+        </div></div>
+        <div class="row"><div class="col-md-12">
+          <h3 class=" text-center">A</h3><hr>
+        </div></div>
+        <div class="col-md-6">
+          <strong>Acquisition</strong>
+          <p>Obtaining control of another corporation by purchasing all or a majority of its outstanding shares, or by purchasing its assets.</p>
+
+          <strong>Administrative Dissolution</strong>
+          <p>An involuntary dissolution of a corporation by an act of the Secretary of State or similar state authority, caused by the corporation's failure to comply with certain statutory requirements; especially the failure to file an annual report, to pay franchise taxes or maintain a valid Registered Agent.</p>
+
+          <strong>Advisory Board of Directors</strong>
+          <p>An advisory board of directors are individuals appointed to advise an elected board of directors. This board is not bound by the duties imposed upon elected board members, and the corporation is not required to follow their recommendations.</p>
+        </div>
+        <div class="col-md-6">
+          <strong>Agent</strong>
+          <p>Anyone who is authorized to act on the behalf of another. A corporation acts only through its agents; therefore, it is important to define what actions an agent is authorized to perform. </p>
+
+          <strong>Agent for Service of Process (aka "resident agent" or "registered agent")</strong>
+          <p>An agent, required to be appointed by a corporation, whose authority is limited to receiving process issued against the corporation. Also known as a Registered Agent or a Resident Agent.</p>
+
+          <strong>Alter Ego</strong>
+          <p>A doctrine of law which disregards the principle of limited liability enjoyed by a corporate entity when it is proven that, in fact, no separate identity of the individual and corporation exists.</p>
+      </div><!-- .row -->
+      <div class="row"><div class="col-md-12"><hr></div></div>
+      <section id="table">
+        <div class="row"><div class="col-md-12 text-center"><h2>Unbeatable Annual Services!</h2></div></div>
+        <div class="row"><div class="col-md-12"><p></p></div></div>
+        <div class="row">
+          <div class="col-md-3 col-sm-6 col-xs-12">
+            <div id="tabledeal">
+              <div id="tablebg">
+                <div class="price-center">
+                  <h3 class="">License Research</h3>
+                  <img src="images/icon1.png" alt="license research">
+                  <img src="images/bluedivider.png" alt="divider">
+                </div> <!-- #price-center -->
+                <div id="price-bullets">
+                  <p class="text-center">You save <strong>$19.10!</strong></p>
+                </div> <!-- #price-center -->
+                <div class="price-center">
+                  <button class="btn btn-md btn-info unbeatable-discounts">Starting at $95 a year</button>
+                </div>
+              </div><!-- #tablebg -->
+            </div><!-- #tabledeal -->
+          </div><!-- col-md-3 -->
+          <div class="col-md-3 col-sm-6 col-xs-12">
+            <div id="tabledeal">
+              <div id="tablebg">
+                <div class="price-center">
+                  <h3 class="">License Filings</h3>
+                  <img src="images/icon2.png" alt="license filings">
+                  <img src="images/bluedivider.png" alt="divider">
+                </div> <!-- #price-center -->
+                <div id="price-bullets">
+                  <p class="text-center">We help you secure and mantain all required business licenses, permits and tax registrations.</p>
+                </div> <!-- #price-center -->
+                <div class="price-center">
+                  <button class="btn btn-md btn-info unbeatable-discounts">$119 per license,<br>per jurisdiction</button>
+                </div>
+              </div><!-- #tablebg -->
+            </div><!-- #tabledeal -->
+          </div><!-- col-md-3 -->
+          <div class="col-md-3 col-sm-6 col-xs-12">
+            <div id="tabledeal">
+              <div id="tablebg">
+                <div class="price-center">
+                  <h3 class="">License Compliance</h3>
+                  <img src="images/icon3.png" alt="license research">
+                  <img src="images/bluedivider.png" alt="divider">
+                </div> <!-- #price-center -->
+                <div id="price-bullets">
+                  <p class="text-center">Manage your entire licensing portfolio.</p>
+                </div> <!-- #price-center -->
+                <div class="price-center">
+                  <button class="btn btn-md btn-info unbeatable-discounts">Starting at $95 a year</button>
+                </div>
+              </div><!-- #tablebg -->
+            </div><!-- #tabledeal -->
+          </div><!-- col-md-3 -->
+          <div class="col-md-3 col-sm-6 col-xs-12">
+            <div id="tabledeal">
+              <div id="tablebg">
+                <div class="price-center">
+                  <h3 class="">License Verification</h3>
+                  <img src="images/icon4.png" alt="license verification">
+                  <img src="images/bluedivider.png" alt="divider">
+                </div> <!-- #price-center -->
+                <div id="price-bullets">
+                  <p class="text-center">Verify that your existing business licenses are valid.</p>
+                </div> <!-- #price-center -->
+                <div class="price-center">
+                  <button class="btn btn-primary btn-info unbeatable-discounts">$35 per license,<br>per jurisdiction</button>
+                </div>
+              </div><!-- #tablebg -->
+            </div><!-- #tabledeal -->
+          </div><!-- col-md-3 -->
+        </div><!-- .row -->
+
+        <div class="row"><div class="col-md-12"><hr></div></div>
+
+        <section id="legal_kits">
+          <div class="row">
+            <div class="col-md-12 text-center">
+              <h2>Corporate Record Kits / Corporate Seals / Stock Certificates</h2>
+            </div>
+          </div>
+          <div class="row"><div class="col-md-12"><p></p></div></div>
+          <div class="row bg-white">
+            <div class="col-md-3 bg-intro-color">
+                <div id="tablebg">
+                  <div class="price-center">
+                    <img src="images/legalkit_1.png" alt="legal kit 1">
+                  </div> <!-- #price-center -->
+                </div><!-- #tablebg -->
+            </div><!-- col-md-3 -->
+            <div class="col-md-9 bg-intro-color">
+              <h3 class="">Attache Legal Kit</h3>
+              <h4>$69</h4>
+              <p>The best legal kit currently on the market, this kit has a high quality black leatherette cover with brass highlights. A sturdy, well-designed presentation package with a handy carrying handle. Special features include pockets for business cards and a flip-out clear vinyl pocket for file reference. This all-in-one case also houses the patented MarkMaker® seal.</p>
+              <button class="btn btn-md btn-info">Order Now!</button>
+            </div>
+          </div><!-- .row -->
+          <div class="row"><div class="col-md-12"><p></p></div></div>
+          <div class="row bg-white">
+            <div class="col-md-3 bg-intro-color">
+                <div id="tablebg">
+                  <div class="price-center">
+                    <img src="images/legalkit_3.png" alt="legal kit 2">
+                  </div> <!-- #price-center -->
+                </div><!-- #tablebg -->
+            </div><!-- col-md-3 -->
+            <div class="col-md-9 bg-intro-color">
+              <h3 class="">Prestige Legal Kit</h3>
+              <h4>$69</h4>
+              <p>This kit offers all of the features and benefits of the Portfolio legal kit but with the Brass index plate on the spine. Special features include handy CD-ROM storage pocket, clear vinyl notepad holder all housed in a full-zippered leatherette case. Traditional coupled with portability and style.</p>
+              <button class="btn btn-md btn-info">Order Now!</button>
+            </div>
+          </div><!-- .row -->
+          <div class="row"><div class="col-md-12"><p></p></div></div>
+          <div class="row bg-white">
+            <div class="col-md-3 bg-intro-color">
+                <div id="tablebg">
+                  <div class="price-center">
+                    <img src="images/legalkit_3.png" alt="legal kit 3">
+                  </div> <!-- #price-center -->
+                </div><!-- #tablebg -->
+            </div><!-- col-md-3 -->
+            <div class="col-md-9 bg-intro-color">
+              <h3 class="">Legacy Legal Kit</h3>
+              <h4>$69</h4>
+              <p>A functional binder and slipcase format comes with all the standard contents as well as your choice of Pocket Seal or Legal Embosser. High-quality burgundy leatherette cover with gold detailing and brass minute book plate permanently adhered to spine for easy identification. Fits standard 12" shelving units.</p>
+              <button class="btn btn-md btn-info">Order Now!</button>
+            </div>
+          </div><!-- .row -->
+        </section>
+
 
 </div>
 
